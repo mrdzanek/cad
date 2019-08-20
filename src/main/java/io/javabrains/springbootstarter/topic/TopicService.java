43b840cad1b8d9e5e473f4760a7cafedf497d1af
DevilMyro <43699114+DevilMyro@users.noreply.github.com>
2019-08-20 21:23:43 +0200

    Initial commit

---
+++ src/main/java/io/javabrains/springbootstarter/topic/TopicService.java
@@ -0,0 +1,37 @@
+package io.javabrains.springbootstarter.topic;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class TopicService {
+
+    @Autowired
+    private TopicRepository topicRepository;
+
+    public List<Topic> getAllTopics() {
+        List<Topic> topics = new ArrayList<>();
+        topicRepository.findAll().forEach(topics::add);
+        return topics;
+    }
+
+    public Topic getTopic(String id) {
+        // other option: return topicRepository.findById(id).orElse(null);
+        return topicRepository.findById(id).get();
+    }
+
+    public void addTopic(Topic topic) {
+        topicRepository.save(topic);
+    }
+
+    public void updateTopic(String id, Topic topic) {
+        topicRepository.save(topic);
+    }
+
+    public void deleteTopic(String id) {
+        topicRepository.deleteById(id);
+    }
+}
